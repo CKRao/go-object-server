80d9b6a579d2f9633fba1a20be68826df68dae8d
clarkrao <clarkrao25@gmail.com>
2019-10-13 01:41:49 +0800

    初始化存储对象CRUD

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-object-server/objects"
+	"log"
+	"net/http"
+)
+
+//go对象存储服务器入口
+func main() {
+	log.Println(">>>>>>>>go-object-server start!>>>>>>>>")
+	//配置路由监听
+	r := gin.Default()
+	//初始化路由配置
+	initRoutes(r)
+
+	err := r.Run(":9000")
+	if err != nil {
+		log.Fatalln(">>>>>>>>go-object-server error!>>>>>>>>>>")
+	}
+
+	log.Println(">>>>>>>>go-object-server end!>>>>>>>>>>")
+}
+
+func initRoutes(r *gin.Engine) {
+	//测试接口
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK,gin.H{
+			"message": "pong",
+		})
+	})
+
+	//对象存储path
+	objectsPath := r.Group("/objects")
+	{
+		//增加存储对象
+		objectsPath.POST("/:name",objects.AddObject)
+		//删除存储对象
+		objectsPath.DELETE("/:name",objects.DeleteObject)
+		//修改存储对象
+		objectsPath.PUT("/:name",objects.UpdateObject)
+		//通过名称获取存储对象
+		objectsPath.GET("/:name",objects.GetObjectByName)
+	}
+}
---
+++ objects/handler.go
@@ -0,0 +1,103 @@
+package objects
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-object-server/util"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const StorageRoot = "F:/objectsstorage"
+
+//通过名称获取存储对象
+func GetObjectByName(c *gin.Context) {
+	c.File(getFilePath(c))
+}
+
+//修改存储对象
+func UpdateObject(c *gin.Context) {
+	filePath := getFilePath(c)
+	//判断文件是否存在
+	if b, err := util.PathExists(filePath); !b || err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "objects is not exists",
+		})
+		return
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "update object error",
+		})
+		return
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, c.Request.Body)
+
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "update object error",
+		})
+	}
+}
+
+//增加存储对象
+func AddObject(c *gin.Context) {
+	filePath := getFilePath(c)
+	//判断文件是否存在
+	if b, err := util.PathExists(filePath); b && err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "objects is exists",
+		})
+		return
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "create file error",
+		})
+		return
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, c.Request.Body)
+
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "add object error",
+		})
+	}
+}
+
+//删除存储对象
+func DeleteObject(c *gin.Context) {
+	filePath := getFilePath(c)
+
+	err := os.Remove(filePath)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "delete error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "delete success",
+	})
+}
+
+func getFilePath(c *gin.Context) string {
+	return StorageRoot + "/objects/" + c.Param("name")
+}
